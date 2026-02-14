294afad00ab569800291b104bd6314f26cbec110
Ganesh-Surya-Manimohan <surya.manimohan@gmail.com>
2026-02-15 01:23:29 +0530

    updated CPU threshold for backend ASG

--- 02-sg/sg.tf
+++ 02-sg/sg.tf
@@ -233,3 +233,13 @@ resource "aws_security_group_rule" "backend_inbound_from_default_VPC" {
   cidr_blocks = ["172.31.0.0/16"]
   security_group_id = module.backend_sg.sg_id
 }
+
+# Added as a part of Jenkins CICD
+resource "aws_security_group_rule" "frontend_inbound_from_default_VPC" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.frontend_sg.sg_id
+}
